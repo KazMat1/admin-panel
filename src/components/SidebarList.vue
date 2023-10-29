44d07dfaab2f649676f2af481b6f4415edb6ffe4
KazMat1 <90205997+KazMat1@users.noreply.github.com>
2023-10-29 19:32:04 +0900

    update a sidebar menu list with icon

--- src/components/SidebarList.vue
+++ src/components/SidebarList.vue
@@ -1,24 +1,30 @@
 <script setup>
-import { reactive } from 'vue';
+import { reactive } from "vue";
+import { ChartBarIcon, ShoppingCartIcon } from "@heroicons/vue/24/outline";
+
+const icons = {
+  ChartBarIcon: ChartBarIcon,
+  ShoppingCartIcon: ShoppingCartIcon,
+};
 
 const lists = reactive([
   {
-    name: 'ダッシュボード',
-    icon: 'TemplateIcon',
-    link: '/',
+    name: "ダッシュボード",
+    icon: "ChartBarIcon",
+    link: "/",
   },
   {
-    name: 'EC',
-    icon: 'ShoppingCartIcon',
-    link: '/#',
+    name: "EC",
+    icon: "ShoppingCartIcon",
+    link: "/#",
     sublists: [
       {
-        name: '商品一覧',
-        link: '/#',
+        name: "商品一覧",
+        link: "/#",
       },
       {
-        name: '注文一覧',
-        link: '/#',
+        name: "注文一覧",
+        link: "/#",
       },
     ],
   },
@@ -27,14 +33,11 @@ const lists = reactive([
 <template>
   <ul class="text-gray-700">
     <li class="mb-1" v-for="list in lists" :key="list.name">
-      <!-- <a
-        :href="list.link"
-        class="block p-2 rounded-sm hover:text-white hover:bg-blue-400"
-      > -->
       <a
         :href="list.link"
-        class="block p-2 rounded-sm text-gray-700 dark:text-gray-300 hover:bg-blue-100 dark:hover:bg-gray-700 hover:text-blue-600 dark:hover:text-blue-600"
+        class="flex items-center p-2 rounded-sm text-gray-700 dark:text-gray-300 hover:bg-blue-100 dark:hover:bg-gray-700 hover:text-blue-600 dark:hover:text-blue-600"
       >
+        <component :is="icons[list.icon]" class="w-6 h-6 mr-2"></component>
         <span>{{ list.name }}</span>
       </a>
     </li>
