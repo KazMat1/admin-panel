cfbcf888fe246f4d6f10b07b2dc862a435b45fdb
KazMat1 <90205997+KazMat1@users.noreply.github.com>
2023-10-29 19:21:41 +0900

    implement a sidebar menu list

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,6 +1,12 @@
-<script setup></script>
+<script setup>
+import SidebarList from './SidebarList.vue';
+
+</script>
 <template>
   <div class="p-4">
     <div class="font-bold text-lg text-blue-600">LOGO</div>
+    <div class="mt-8">
+        <SidebarList />
+    </div>
   </div>
 </template>
---
+++ src/components/SidebarList.vue
@@ -0,0 +1,42 @@
+<script setup>
+import { reactive } from 'vue';
+
+const lists = reactive([
+  {
+    name: 'ダッシュボード',
+    icon: 'TemplateIcon',
+    link: '/',
+  },
+  {
+    name: 'EC',
+    icon: 'ShoppingCartIcon',
+    link: '/#',
+    sublists: [
+      {
+        name: '商品一覧',
+        link: '/#',
+      },
+      {
+        name: '注文一覧',
+        link: '/#',
+      },
+    ],
+  },
+]);
+</script>
+<template>
+  <ul class="text-gray-700">
+    <li class="mb-1" v-for="list in lists" :key="list.name">
+      <!-- <a
+        :href="list.link"
+        class="block p-2 rounded-sm hover:text-white hover:bg-blue-400"
+      > -->
+      <a
+        :href="list.link"
+        class="block p-2 rounded-sm text-gray-700 dark:text-gray-300 hover:bg-blue-100 dark:hover:bg-gray-700 hover:text-blue-600 dark:hover:text-blue-600"
+      >
+        <span>{{ list.name }}</span>
+      </a>
+    </li>
+  </ul>
+</template>
