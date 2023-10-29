63483468b6b62ab9e1a2449d88d74694aa9fdbd3
KazMat1 <90205997+KazMat1@users.noreply.github.com>
2023-10-29 17:11:50 +0900

    Tailwindの動作確認完了

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  darkMode: false, // or 'media' or 'class'
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
