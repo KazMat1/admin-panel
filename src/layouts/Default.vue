29ded52b591ba22959455c91cb6ace7e118804b1
KazMat1 <90205997+KazMat1@users.noreply.github.com>
2023-10-29 18:30:30 +0900

    implemtn dark mode

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,7 +1,7 @@
 <script setup>
-import { Bars3Icon } from "@heroicons/vue/24/outline";
+import { Bars3Icon, SunIcon, MoonIcon } from "@heroicons/vue/24/outline";
 import { onMounted, onUnmounted, ref } from "vue";
-import { debounce } from 'lodash';
+import { debounce } from "lodash";
 
 const innerWidth = ref(window.innerWidth);
 const show = ref(innerWidth.value >= 1280 ? true : false);
@@ -9,6 +9,29 @@ const toggleShow = () => {
   show.value = !show.value;
 };
 
+const theme = ref("light");
+
+// check the theme a user select and add class for theme to html element
+if (localStorage.theme === 'dark') {
+  document.documentElement.classList.add('dark');
+  theme.value = 'dark';
+} else {
+  document.documentElement.classList.remove('dark');
+  theme.value = 'light';
+}
+
+// change theme color method
+const changeMode = (mode) => {
+  theme.value = mode
+  // add class to html element
+  theme.value === 'light'
+    ? document.documentElement.classList.remove('dark')
+    : document.documentElement.classList.add('dark');
+  // set a parameter in local storage
+    localStorage.theme = mode;
+}
+
+// check window size method
 const checkWindowSize = () => {
   if (window.innerWidth >= 1280) {
     if (show.value === false && innerWidth.value < 1280) show.value = true;
@@ -18,37 +41,52 @@ const checkWindowSize = () => {
   innerWidth.value = window.innerWidth;
 };
 onMounted(() => {
-  // resizeイベントで、windowサイズの変更を検知する。debounceで、0.1秒経過して、チェック
-  window.addEventListener('resize', debounce(checkWindowSize, 100));
+  // detect the size of window from resize event. check it with debounce method per 0.1s
+  window.addEventListener("resize", debounce(checkWindowSize, 100));
 });
 onUnmounted(() => {
-  window.removeEventListener('resize', checkWindowSize);
+  // not required to check the window size because of unmounted
+  window.removeEventListener("resize", checkWindowSize);
 });
 </script>
 <template>
   <div class="relative">
     <div
-      class="fixed top-0 w-64 h-screen bg-white z-20 transform duration-300"
+      class="fixed top-0 w-64 h-screen bg-white dark:bg-gray-800 z-20 transform duration-300 dark:text-gray-300"
       :class="{ '-translate-x-full': !show }"
     >
       サイドバー
     </div>
     <div
-      class="fixed xl:hidden inset-0 bg-gray-900 opacity-50 z-10 duration-300"
+      class="fixed xl:hidden inset-0 bg-gray-900 z-10 opacity-50"
       @click="toggleShow"
       v-show="show"
     ></div>
     <div
-      class="bg-gray-100 h-screen overflow-hidden"
+      class="bg-gray-100 dark:bg-gray-900 h-screen overflow-hidden duration-300"
       :class="{ 'xl:pl-64': show }"
     >
-      <div class="bg-white rounded shadow m-4 p-4">
+      <div
+        class="flex items-center justify-between bg-white dark:bg-gray-800 rounded shadow m-4 p-4"
+      >
         <Bars3Icon
-          class="h-6 w-6 text-gray-600 cursor-pointer"
+          class="h-6 w-6 text-gray-600 dark:text-gray-300 cursor-pointer"
           @click="toggleShow"
         />
+        <div>
+          <MoonIcon
+            class="w-7 h-7 text-gray-600 cursor-pointer"
+            @click="changeMode('dark')"
+            v-if="theme === 'light'"
+          />
+          <SunIcon
+            class="w-7 h-7 text-gray-300 cursor-pointer"
+            @click="changeMode('light')"
+            v-else
+          />
+        </div>
       </div>
-      <div>
+      <div class="dark:text-gray-300">
         <slot />
       </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'class', // or 'media' or 'class'
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {},
